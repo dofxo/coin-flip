1ca34d912e5ad82e04b4425669f9b620ebd70be4
Mohammad Kargar <kargat504@gmail.com>
2022-08-03 20:01:56 -0700

    added the counter for each turn

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ const resultEl = document.getElementById('result')
 const startBtn = document.getElementById('start')
 const loadingEl = document.getElementById('loading')
 // event listener for start button
-startBtn.addEventListener('click', function () {
-    hideResult()
-    unHideLoading()
-    setTimeout(hideLoading, 850)
-    setTimeout(showResult, 850)
 
+    startBtn.addEventListener('click', function () {
+
+  
+        hideResult()
+        unHideLoading()
+        setTimeout(hideLoading, 300)
+        setTimeout(showResult, 300)
+ 
 
 })
+
+
 // shows the loading animation
 function unHideLoading() {
     loadingEl.style.display = 'flex'
@@ -29,7 +34,6 @@ the #result element
 */
 function showResult() {
     let turn = Math.floor(Math.random() * 10) + 1
-    console.log(turn);
     if (turn > 5) {
         resultEl.textContent = 'شیر'
         resultEl.style.animation = 'none'
@@ -42,3 +46,30 @@ function showResult() {
 
     }
 }
+// shows and calculates each turn user takes.
+
+const countResultEl = document.getElementById('count-number')
+const countEl = document.getElementById('count')
+
+countResultEl.textContent = localStorage.getItem('count')
+let count = JSON.parse(localStorage.getItem('count'))
+startBtn.addEventListener('click', function () {
+    count += 1
+    countResultEl.textContent = count
+    // sends the value to local storage
+    localStorage.setItem('count', count)
+
+})
+
+// resets the count from storage
+
+const resetBtn = document.getElementById('reset')
+
+resetBtn.addEventListener('click', function () {
+    localStorage.clear()
+    countResultEl.textContent = ''
+    count = 0
+    resultEl.style.display = 'none'
+
+})
+
